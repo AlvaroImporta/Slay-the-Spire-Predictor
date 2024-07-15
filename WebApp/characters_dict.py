bf3a3952244934efc4cfd7556a52e878607be411
AlvaroImporta <alv.zubi@gmail.com>
2024-07-15 17:49:53 +0200

    cambios en la web app

---
+++ WebApp/characters_dict.py
@@ -0,0 +1,46 @@
+characters_dict = {
+    'Ironclad': {
+        'max_hp': 80,
+        'model': 'extra_trees_ironclad.pkl',
+        'cards': {
+            'Strike': 'https://example.com/ironclad/strike.jpg',
+            'Defend': 'https://example.com/ironclad/defend.jpg',
+            'Bash': 'https://example.com/ironclad/bash.jpg'
+        },
+        'initial_cards': ['Strike', 'Defend'],
+        'initial_relic': 'Burning Blood'
+    },
+    'Silent': {
+        'max_hp': 70,
+        'model': 'extra_trees_silent.pkl',
+        'cards': {
+            'Strike': 'https://example.com/silent/strike.jpg',
+            'Defend': 'https://example.com/silent/defend.jpg',
+            'Survivor': 'https://example.com/silent/survivor.jpg'
+        },
+        'initial_cards': ['Strike', 'Defend'],
+        'initial_relic': 'Ring of the Snake'
+    },
+    'Defect': {
+        'max_hp': 75,
+        'model': 'extra_trees_defect.pkl',
+        'cards': {
+            'Strike': 'https://example.com/defect/strike.jpg',
+            'Defend': 'https://example.com/defect/defend.jpg',
+            'Zap': 'https://example.com/defect/zap.jpg'
+        },
+        'initial_cards': ['Strike', 'Defend'],
+        'initial_relic': 'Cracked Core'
+    },
+    'Watcher': {
+        'max_hp': 75,
+        'model': 'extra_trees_watcher.pkl',
+        'cards': {
+            'Strike': 'https://example.com/watcher/strike.jpg',
+            'Defend': 'https://example.com/watcher/defend.jpg',
+            'Eruption': 'https://example.com/watcher/eruption.jpg'
+        },
+        'initial_cards': ['Strike', 'Defend'],
+        'initial_relic': 'Pure Water'
+    }
+}
---
+++ WebApp/relics_dict.py
@@ -0,0 +1,7 @@
+relics_dict = {
+    'relic1': 'https://example.com/relic/w12312.jpg',
+    'relic2': 'https://example.com/relic/346568.jpg',
+    'relic3': 'https://example.com/relic/6778998.jpg',
+    'relic4': 'https://example.com/relic/894564.jpg',
+    'relic5': 'https://example.com/relic/6778998.jpg'
+}
