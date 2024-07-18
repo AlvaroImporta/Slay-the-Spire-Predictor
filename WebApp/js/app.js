d44b4833c5b3b1f082724df9124a9f4bba2b94c7
AlvaroImporta <alv.zubi@gmail.com>
2024-07-18 15:54:05 +0200

    cambios en funcionalidad de lista cartas y reliquias

--- WebApp/js/app.js
+++ WebApp/js/app.js
@@ -16,38 +16,73 @@ function LoadCharacterData() {
     document.getElementById('entering-hp').value = character_data.max_hp;
     
     const card_list = document.getElementById('card-list');
-    card_list.innerHTML = '';
+    const relic_list = document.getElementById('relic-list');
 
     character_data.initial_cards.forEach(card => {
         const li = document.createElement('li');
         li.textContent = card;
+        li.className = 'card-item';
 
         const button = document.createElement('button');
         button.textContent = 'X';
         button.onclick = () => {
             card_list.removeChild(li);
         };
+        const buttonlevel = document.createElement('button');
+        buttonlevel.textContent = '↑';
+        buttonlevel.onclick = () => {
+            li.textContent = card + '+1';
+            const button = document.createElement('button');
+            button.textContent = 'X';
+            button.onclick = () => {
+                card_list.removeChild(li);
+            };
+            li.appendChild(button);
+        };
 
+        li.appendChild(buttonlevel);
         li.appendChild(button);
         card_list.appendChild(li);
     });
-    // card_list.forEach(element => {
-    //     document.getElementById('card-list').appendChild = '<li>'+element+'<button>X</button></li>';
-    // });
-       
-    
+   
+    const li = document.createElement('li');
+    li.textContent = character_data.initial_relic;
+    li.className = 'relic-item';
+
+    const button = document.createElement('button');
+    button.textContent = 'X';
+    button.onclick = () => {
+        relic_list.removeChild(li);
+    };
+
+    li.appendChild(button);
+    relic_list.appendChild(li);
+
+    return character_data;
 }
 
+
 function chooseBetterCards() {
-    const maxHP = document.getElementById('max-hp').textContent;
-    const enteringHP = document.getElementById('entering-hp').textContent;
+    const maxHP = document.getElementById('max-hp').value;
+    const enteringHP = document.getElementById('entering-hp').value;
 
     const cardItems = document.querySelectorAll('.card-item');
-    const cards = Array.from(cardItems).map(item => item.textContent.replace('↑✗', ''));
+    const cards = []
+    cardItems.forEach(item => {
+        let textNode = item.firstChild;
+        cards.push(textNode.textContent.replace('↑✗', ''));
+    });
 
-    const card1 = document.getElementById('card1-search').value;
-    const card2 = document.getElementById('card2-search').value;
-    const card3 = document.getElementById('card3-search').value;
+    const relicItems = document.querySelectorAll('.relic-item');
+    const relics = []
+    relicItems.forEach(item => {
+        let textNode = item.firstChild;
+        relics.push(textNode.textContent.replace('↑✗', ''));
+    });
+
+    const card1 = document.getElementById('card1').alt;
+    const card2 = document.getElementById('card2').alt;
+    const card3 = document.getElementById('card3').alt;
 
     const cardOptions = [
         [...cards, card1],
@@ -62,10 +97,11 @@ function chooseBetterCards() {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
+            character: GetCharacter(),
             max_hp: maxHP,
             entering_hp: enteringHP,
             card_options: cardOptions,
-            relics: character.initial_relic
+            relics: relics
         })
     })
     .then(response => response.json())
@@ -81,32 +117,32 @@ function chooseBetterCards() {
 function clearCards() {
     document.getElementById('card1-search').value = '';
     document.getElementById('card1-image').src = '';
-    document.getElementById('card1-prediction').textContent = '0.0';
+    document.getElementById('card1-prediction').textContent = '';
     
     document.getElementById('card2-search').value = '';
     document.getElementById('card2-image').src = '';
-    document.getElementById('card2-prediction').textContent = '0.0';
+    document.getElementById('card2-prediction').textContent = '';
     
     document.getElementById('card3-search').value = '';
     document.getElementById('card3-image').src = '';
-    document.getElementById('card3-prediction').textContent = '0.0';
+    document.getElementById('card3-prediction').textContent = '';
 
-    document.getElementById('skip-prediction').textContent = 'Skip: 0.0';
+    document.getElementById('skip-prediction').textContent = '';
 }
 
 
 function clearRelics() {
     document.getElementById('relic1-search').value = '';
     document.getElementById('relic1-image').src = '';
-    document.getElementById('relic1-prediction').textContent = '0.0';
+    document.getElementById('relic1-prediction').textContent = '';
     
     document.getElementById('relic2-search').value = '';
     document.getElementById('relic2-image').src = '';
-    document.getElementById('relic2-prediction').textContent = '0.0';
+    document.getElementById('relic2-prediction').textContent = '';
     
     document.getElementById('relic3-search').value = '';
     document.getElementById('relic3-image').src = '';
-    document.getElementById('relic3-prediction').textContent = '0.0';
+    document.getElementById('relic3-prediction').textContent = '';
 
-    document.getElementById('relic-skip-prediction').textContent = 'Skip: 0.0';
+    document.getElementById('relic-skip-prediction').textContent = '';
 }
